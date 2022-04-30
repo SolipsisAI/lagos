896941c0da44ecca4592ce969fe195fdd8fb5d1b
Bitjockey <fastjack@jackpoint.network>
2022-04-30 17:14:33 -0400

    update README and use wikipediaapi library

--- README.md
+++ README.md
@@ -4,5 +4,20 @@
 
 ```shell
 pdm install
+pdm install -G dev
+
+# Install dependencies for macos
+pdm install -G macos
 ```
 
+### Visual Studio Code
+
+Put this in `.vscode/settings.json`:
+
+```json
+{
+    "python.analysis.extraPaths": [
+        "./__pypackages__/3.9/lib"
+    ]
+}
+```
--- lagos/core.py
+++ lagos/core.py
@@ -1,47 +1,14 @@
 # -*- coding: utf-8 -*-
-from typing import Union, List, Dict
-
-import tensorflow as tf
+import wikipediaapi
 
 from lagos.utils import sanitize
 
 
-WIKI_URL = "https://en.wikipedia.org/wiki"
-API_URL = "https://en.wikipedia.org/w/api.php"
+def get_article(title) -> str:
+    wiki = wikipediaapi.Wikipedia(
+        "en",
+        extract_format=wikipediaapi.ExtractFormat.WIKI,
+    )
+    page = wiki.page(sanitize(title))
 
-
-def get_articles(*titles):
-    params = {
-        "action": "query",
-        "format": "json",
-        "prop": "extracts",
-        "explaintext": True,
-        "exsectionformat": "plain",
-    }
-
-    params["titles"] = ("|".join(list(map(sanitize, titles))),)
-
-    if len(titles) > 1:
-        params["exintro"] = True
-
-    data = get_page(f"{API_URL}", format="json", params=params)
-    results = data["query"]["pages"]
-    pages = [page.get("extract", "") for _, page in results.items()]
-    text = "".join(pages)
-
-    return "\n".join(text.split("\n"))
-
-
-def get_page(url: str, format: str = "json", params=None) -> Union[str, Dict, None]:
-    if params is None:
-        params = {"action": "raw"}
-
-    r = requests.get(url, params=params)
-
-    if r.status_code >= 300:
-        return None
-
-    if format != "json":
-        return r.text
-
-    return r.json()
+    return page
