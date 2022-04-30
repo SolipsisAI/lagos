41c65aa0b9da2b10ae18e67a64a86b991be9eafe
Bitjockey <fastjack@jackpoint.network>
2022-04-30 17:31:23 -0400

    Method to traverse sections

--- lagos/core.py
+++ lagos/core.py
@@ -4,7 +4,19 @@ import wikipediaapi
 from lagos.utils import sanitize
 
 
-def get_article(title) -> str:
+def section_text(
+    section: wikipediaapi.WikipediaPageSection,
+    level: int = 0,
+):
+    if not section.sections:
+        return section.text
+
+    level += 1
+
+    return section.text + section_text(section.sections[level], level)
+
+
+def get_page(title) -> str:
     wiki = wikipediaapi.Wikipedia(
         "en",
         extract_format=wikipediaapi.ExtractFormat.WIKI,
