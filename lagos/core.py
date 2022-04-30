76bff0b19ceb424c9585ae9b4e0edc3330798c63
Bitjockey <bitjockey@jackpoint.network>
2022-04-30 13:10:36 -0400

    Extract Page text

--- lagos/core.py
+++ lagos/core.py
@@ -8,19 +8,43 @@ from pandoc import types
 from lagos.utils import sanitize
 
 WIKI_URL = "https://en.wikipedia.org/wiki"
+API_URL = "https://en.wikipedia.org/w/api.php"
 
 
-def get_article(title: str):
-    raw_article = get_page(f"{WIKI_URL}/{sanitize(title)}")
-    if raw_article is None:
-        return "ERROR: Could not fetch"
-    article_document = pandoc.read(source=raw_article, format="mediawiki")
-    return article_document
+def get_article(title: str, response_only: bool = False):
+    params = {
+        "action": "query",
+        "format": "json",
+        "titles": title,
+        "prop": "extracts",
+        "explaintext": True,
+        "exsectionformat": "plain",
+    }
+
+    data = get_page(f"{API_URL}", params=params, response_only=response_only)
+    results = data["query"]["pages"]
+    pages = [page["extract"] for pageid, page in results.items()]
+
+    text = "".join(pages)
+
+    return "\n".join(text.split("\n"))
 
 
-def get_page(url: str) -> Union[str, None]:
-    params = {"action": "raw"}
+def get_page(
+    url: str, format: str = "json", params=None, response_only: bool = False
+) -> Union[str, None]:
+    if params is None:
+        params = {"action": "raw"}
+
     r = requests.get(url, params=params)
+
+    if response_only:
+        return r
+
     if r.status_code >= 300:
         return None
-    return r.text
+
+    if format != "json":
+        return r.text
+
+    return r.json()
