2aa0977e7cda31625a61a849f029d8284fb8c92e
Bitjockey <fastjack@jackpoint.network>
2022-05-01 15:58:36 -0400

    initial pipeline use for the client

--- lagos/client.py
+++ lagos/client.py
@@ -6,34 +6,37 @@ import websockets
 
 from lagos.pipelines import load_pipeline
 
-CONNECTIONS = set()
+BOT_USER = "chatbot"
 
 
-def bot_handler(pipeline, event):
+async def bot_handler(websocket, pipeline, event):
     _, conversation = pipeline.predict(text=event["text"])
     response_text = conversation.generated_responses[-1]
 
     # Show typing
-    websockets.broadcast(
-        CONNECTIONS, json.dumps({"user": "chatbot", "is_typing": True})
+    await websocket.send(
+        json.dumps({"user": BOT_USER, "is_typing": True})
     )
     num_tokens = len(response_text)
     time.sleep(0.1 * num_tokens)
 
-    # Chatbot
-    websockets.broadcast(
-        CONNECTIONS,
+    # Send response
+    await websocket.send(
         json.dumps(
             {
-                "user": "chatbot",
+                "user": BOT_USER,
                 "text": response_text,
                 "is_typing": False,
             }
-        ),
+        )
     )
 
 
 async def bot(pipeline_name, connect_url):
+    pipeline = load_pipeline(pipeline_name)
     async with websockets.connect(connect_url) as websocket:
-        await websocket.send(json.dumps({"user": "chatbot", "text": f"model: {pipeline_name}"}))
-        await websocket.recv()
+        async for message in websocket:
+            event = json.loads(message)
+            if event["user"] != BOT_USER:
+                await bot_handler(websocket, pipeline, event)
+            await websocket.recv()
