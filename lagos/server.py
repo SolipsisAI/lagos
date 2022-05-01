f2364985dd027e33dbe2ae23451d74455e087c85
Bitjockey <fastjack@jackpoint.network>
2022-05-01 02:13:20 -0400

    Remove conversation_id

--- lagos/server.py
+++ lagos/server.py
@@ -7,9 +7,9 @@ from lagos.pipelines import load_pipeline
 CONNECTIONS = set()
 
 
-def bot_handler(pipeline, conversation_id, message):
-    conversation_id, conversation = pipeline.predict(conversation_id, message)
-    return conversation_id, conversation.generated_responses[-1] 
+def bot_handler(pipeline, message):
+    _, conversation = pipeline.predict(text=message)
+    return conversation.generated_responses[-1]
 
 
 def handler_wrapper(pipeline_name):
@@ -23,16 +23,16 @@ def handler_wrapper(pipeline_name):
             async for message in websocket:
                 # Broadcast a message to all connected clients.
                 websockets.broadcast(CONNECTIONS, message)
-                conversation_id, response = bot_handler(pipeline, conversation_id, message)
+                response = bot_handler(pipeline, message)
                 websockets.broadcast(CONNECTIONS, response)
         finally:
             # Unregister.
             CONNECTIONS.remove(websocket)
-    
+
     return handler
 
 
-async def app(pipeline_name, host = "", port: int = 8001):
+async def app(pipeline_name, host="", port: int = 8001):
     print(f"MODEL: {pipeline_name}")
     async with websockets.serve(handler_wrapper(pipeline_name), host, port):
         print(f"Connect to: ws://{host}:{port}")
