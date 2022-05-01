4a4592331f236de6b53206d5135733ffedae8163
Bitjockey <fastjack@jackpoint.network>
2022-04-30 22:58:00 -0400

    Fix conversational pipeline

--- lagos/pipelines/conversational.py
+++ lagos/pipelines/conversational.py
@@ -11,18 +11,19 @@ class Conversational(BasePipeline):
     def __init__(self, device: int = -1):
         super().__init__("conversational", OPTIONS, device)
         self.context = {}
-    
+
     def add_context(self, conversation_id, text: str):
         if conversation_id not in self.context:
-            conversation = Conversation() 
-            conversation_id = conversation.uuid
+            conversation = Conversation()
+            conversation_id = str(conversation.uuid)
             self.context[conversation_id] = conversation
         self.context[conversation_id].add_user_input(text)
         return conversation_id
 
     def get_context(self, conversation_id):
         return self.context[conversation_id]
-    
+
     def predict(self, conversation_id=None, text=None):
         conversation_id = self.add_context(conversation_id, text)
-        return self.get_context(conversation_id)
+        context = self.get_context(conversation_id)
+        return self.pipeline(context)
