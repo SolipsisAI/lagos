d2b3f75e3477604f2627c260bfef02fb70892fbf
Bitjockey <fastjack@jackpoint.network>
2022-04-30 23:20:01 -0400

    Return convesrsation_id as string

--- lagos/pipelines/conversational.py
+++ lagos/pipelines/conversational.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from transformers import Conversation
 
 from .base import BasePipeline
@@ -12,7 +14,7 @@ class Conversational(BasePipeline):
         super().__init__("conversational", OPTIONS, device)
         self.context = {}
 
-    def add_context(self, conversation_id, text: str):
+    def add_context(self, conversation_id: str, text: str):
         if conversation_id not in self.context:
             conversation = Conversation()
             conversation_id = str(conversation.uuid)
@@ -20,10 +22,12 @@ class Conversational(BasePipeline):
         self.context[conversation_id].add_user_input(text)
         return conversation_id
 
-    def get_context(self, conversation_id):
+    def get_context(self, conversation_id: str):
         return self.context[conversation_id]
 
-    def predict(self, conversation_id=None, text=None):
+    def predict(
+        self, conversation_id: str = None, text: str = None
+    ) -> Tuple[str, Conversation]:
         conversation_id = self.add_context(conversation_id, text)
         context = self.get_context(conversation_id)
-        return self.pipeline(context)
+        return conversation_id, self.pipeline(context)
