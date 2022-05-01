3edb648bf2a9afc595d09f6ad52efd2cc38eab04
Bitjockey <fastjack@jackpoint.network>
2022-05-01 18:13:37 -0400

    Make the query more robust

--- lagos/data_source/base.py
+++ lagos/data_source/base.py
@@ -7,15 +7,25 @@ class BaseDataSource:
         self.results = {}
 
     def query(self, key, update: bool = False):
-        if key not in self.results or update:
-            self.results[key] = self.find(key)
+        key_parts = self.sanitize(key).split("|")
+        root_key = key_parts[0]
+
+        if root_key not in self.results or update:
+            self.results[root_key] = self.find(root_key)
+
         return self.by_key(key)
 
     def find(self, key, exclude, flatten):
         raise NotImplementedError
 
     def by_key(self, key):
-        return self.results.get(key)
+        key_parts = self.sanitize(key).split("|")
+        root_key = key_parts[0]
+
+        if len(key_parts) == 1:
+            return self.results.get(root_key)
+
+        return self.results.get(root_key, {}).get("|".join(key_parts))
 
     def sanitize(self, key):
         raise NotImplementedError
