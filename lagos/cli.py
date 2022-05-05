01aff172930b12c71d4ddbec80d59df1e8fec7f6
Bitjockey <bitjockey@jackpoint.network>
2022-05-05 00:32:49 -0400

    Don't import everything in cli

--- lagos/cli.py
+++ lagos/cli.py
@@ -3,9 +3,6 @@
 import asyncio
 import click
 
-from lagos.server import app
-from lagos.client import bot
-
 
 @click.group()
 def cli():
@@ -17,6 +14,7 @@ def cli():
 @click.option("--connect", "-c", default="ws://localhost:8001")
 def start(pipeline_name, connect):
     """Start chatbot"""
+    from lagos.client import bot
     asyncio.run(bot(pipeline_name, connect_url=connect))
 
 
@@ -25,4 +23,5 @@ def start(pipeline_name, connect):
 @click.option("--port", "-P", default=8001, help="port")
 def serve(host, port):
     """Start websocket"""
+    from lagos.server import app
     asyncio.run(app(host, port))
